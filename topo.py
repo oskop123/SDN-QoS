5d53feda6be8b6e782658db0ce7c4aadd2a7766e
Oskar Swierczek <oswierczek99@gmail.com>
2022-04-10 16:13:49 +0200

    iperf3 and ffmpeg traffic generators

--- topo.py
+++ topo.py
@@ -8,6 +8,7 @@ from mininet.log import setLogLevel
 from mininet.cli import CLI
 from mininet.link import TCLink
 
+import time
 
 class CustomTopo(Topo):
     def build(self):
@@ -17,10 +18,16 @@ class CustomTopo(Topo):
         h2 = self.addHost('h2')
         h3 = self.addHost('h3')
 
+        # self.addLink(s1, s2, bw=10)
+        # self.addLink(h1, s1, bw=10)
+        # self.addLink(h2, s1, bw=10)
+        # self.addLink(h3, s2, bw=10)
+
         self.addLink(s1, s2)
         self.addLink(h1, s1)
         self.addLink(h2, s1)
         self.addLink(h3, s2)
+
 
 
 class RyuController(Controller):
@@ -32,6 +39,27 @@ class RyuController(Controller):
         self.cmd("ryu-manager ryu.app.rest_qos ryu.app.qos_simple_switch_13 " \
             "ryu.app.rest_conf_switch &")
 
+def measurements(net):
+    s1 = net.get('s1')
+    h1 = net.get('h1')
+    h2 = net.get('h2')
+    h3 = net.get('h3')
+
+    s1.cmd('./configure_queues.sh')
+
+    # h3.cmd('iperf -s -u -i 1 -p 5001 &')
+    # h2.cmd('iperf -c 10.0.0.3 -p 5001 -u -b 1G -t 20 &')
+
+    h3.cmd('iperf -s -i 1 -p 5001 &')
+    h2.cmd('iperf -c 10.0.0.3 -p 5001 -b 1G -t 20 &')
+
+    h3.cmd('ffplay udp://10.0.0.3:1234 &')
+
+    time.sleep(5)
+
+    h1.cmd('ffmpeg -i Big_Buck_Bunny_1080_10s_30MB.mp4 -f mpegts udp://10.0.0.3:1234')
+
+    time.sleep(15)
 
 if __name__ == '__main__':
     setLogLevel('info')
@@ -54,5 +82,11 @@ if __name__ == '__main__':
     print( "Testing network connectivity" )
     net.pingAll()
 
-    CLI(net)
+    print("wait")
+    time.sleep(10)
+    #CLI(net)
+
+    measurements(net)
+
     net.stop()
+
